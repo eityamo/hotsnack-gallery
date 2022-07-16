441bc2ca9981d6886617a64af7211a932c33fbc4
eityamo <eityamo@gmail.com>
2022-07-16 18:38:11 +0900

    add:vue-router設定

---
+++ app/javascript/pages/top/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>hotsnack</p>
+</template>
+
+<script>
+export default {
+  name: "TopIndex",
+  data() {
+    return {
+      title: "ホットスナック美術館",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
