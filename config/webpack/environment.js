8f65cf97f9a1f8ec59ffbc95a08efaea94b74399
eityamo <eityamo@gmail.com>
2022-07-16 18:10:57 +0900

    add:vueでホーム画面の初期設定

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,8 @@
+// vue-loaderの読み込みを行う記載
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
