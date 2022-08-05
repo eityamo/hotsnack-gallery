23a2a931727744c3c680bb553501699cd637265d
eityamo <eityamo@gmail.com>
2022-08-05 15:17:19 +0900

    add:transitionによるアニメーションの追加

--- app/javascript/components/layout/BaseLayout.vue
+++ app/javascript/components/layout/BaseLayout.vue
@@ -2,7 +2,7 @@
     <v-app>
         <the-header />
         <v-main>
-            <transition name="slide" mode="out-in">
+            <transition name="fade" mode="out-in">
                 <router-view />
             </transition>
             <v-row class="ma-6" />
@@ -23,3 +23,20 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+.fade-enter-active {
+    transition: all 0.5s ease;
+}
+.fade-leave-active {
+    transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.fade-enter {
+    transform: translateX(15px);
+    opacity: 0;
+}
+.fade-leave-to {
+    transform: translateX(-10px);
+    opacity: 0;
+}
+</style>
