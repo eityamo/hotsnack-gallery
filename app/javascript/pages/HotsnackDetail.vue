bcd706b207686fd9a439bcdde77989ad652247c7
eityamo <eityamo@gmail.com>
2022-08-06 11:27:22 +0900

    fix:いいね機能の仕様変更

--- app/javascript/pages/HotsnackDetail.vue
+++ app/javascript/pages/HotsnackDetail.vue
@@ -1,121 +1,126 @@
 <template>
     <base-container>
-        <v-card class="mx-auto" max-width="375" color="#f6f5ee" outlined v-if="'id' in hotsnack">
-            <v-row justify="center" align="center" class="mt-8 mx-4">
-                <v-col cols="6" class="pa-0">
-                    <v-card outlined color="#f6f5ee">
-                        <div class="text-center">
-                            <v-btn text class="text-h6">査定額</v-btn>
-                        </div>
-                    </v-card>
-                </v-col>
-                <v-col class="pa-0">
-                    <v-card outlined color="#f6f5ee">
-                        <div class="text-center">
-                            <v-btn icon>
-                                <v-icon icon @click="addLikeCount">mdi-thumb-up</v-icon>
-                            </v-btn>
-                        </div>
-                    </v-card>
-                </v-col>
-                <v-col class="pa-0">
-                    <v-card outlined color="#f6f5ee">
-                        <div class="text-center">
-                            <v-btn icon>
-                                <v-icon icon @click="addDislikeCount">mdi-thumb-down</v-icon>
-                            </v-btn>
-                        </div>
-                    </v-card>
-                </v-col>
-            </v-row>
-            <v-row justify="center" align="center" class="mb-4 mx-4">
-                <v-col cols="6" class="pa-0">
-                    <v-card outlined color="#f6f5ee">
-                        <div class="text-center">{{ multiplyLikeCountAndPrice }}</div>
-                    </v-card>
-                </v-col>
-                <v-col class="pa-0">
-                    <v-card outlined color="#f6f5ee">
-                        <div class="text-center">{{ hotsnack.like_count }}</div>
-                    </v-card>
-                </v-col>
-                <v-col class="pa-0">
-                    <v-card outlined color="#f6f5ee">
-                        <div class="text-center">{{ hotsnack.dislike_count }}</div>
-                    </v-card>
-                </v-col>
-            </v-row>
+        <base-transition>
+            <v-card class="mx-auto" max-width="375" color="#f6f5ee" outlined v-if="!isLoading && hotsnack.id">
+                <v-row justify="center" align="center" class="mt-8 mx-4">
+                    <v-col cols="6" class="pa-0">
+                        <v-card outlined color="#f6f5ee">
+                            <div class="text-center">
+                                <v-btn text class="text-h6">査定額</v-btn>
+                            </div>
+                        </v-card>
+                    </v-col>
+                    <v-col class="pa-0">
+                        <v-card outlined color="#f6f5ee">
+                            <div class="text-center">
+                                <v-btn icon>
+                                    <v-icon icon @click="addLikeCount">mdi-thumb-up</v-icon>
+                                </v-btn>
+                            </div>
+                        </v-card>
+                    </v-col>
+                    <v-col class="pa-0">
+                        <v-card outlined color="#f6f5ee">
+                            <div class="text-center">
+                                <v-btn icon>
+                                    <v-icon icon @click="addDislikeCount">mdi-thumb-down</v-icon>
+                                </v-btn>
+                            </div>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                <v-row justify="center" align="center" class="mb-4 mx-4">
+                    <v-col cols="6" class="pa-0">
+                        <v-card outlined color="#f6f5ee">
+                            <div class="text-center">{{ multiplyLikeCountAndPrice }}</div>
+                        </v-card>
+                    </v-col>
+                    <v-col class="pa-0">
+                        <v-card outlined color="#f6f5ee">
+                            <div class="text-center">{{ hotsnack.like_count }}</div>
+                        </v-card>
+                    </v-col>
+                    <v-col class="pa-0">
+                        <v-card outlined color="#f6f5ee">
+                            <div class="text-center">{{ hotsnack.dislike_count }}</div>
+                        </v-card>
+                    </v-col>
+                </v-row>
 
-            <hr class="hr1" />
+                <hr class="hr1" />
 
-            <v-card class="ma-10" color="#f6f5ee" outlined>
-                <v-container class="outline">
-                    <v-container class="inline">
-                        <v-container class="baseline">
-                            <v-container class="border">
-                                <v-img :src="hotsnack.image" />
+                <v-card class="ma-10" color="#f6f5ee" outlined>
+                    <v-container class="outline">
+                        <v-container class="inline">
+                            <v-container class="block">
+                                <v-container class="border">
+                                    <v-img :src="hotsnack.image" />
+                                </v-container>
                             </v-container>
                         </v-container>
                     </v-container>
-                </v-container>
 
-                <v-card raised elevation="8" tile class="mx-10 my-10">
-                    <v-col align="center" class="s-font pt-2 pb-0">{{ hotsnack.name }}</v-col>
-                    <v-col align="center" class="xs-font pt-1 pb-2">{{ hotsnack.price }}円（税込）</v-col>
-                    <hr class="hr2" />
-                    <v-col align="center" class="s-font pt-2 pb-0">{{ hotsnack.store }}</v-col>
-                    <v-col align="center" class="xs-font pt-1 pb-0" v-if="hotsnack.store === 'セブン-イレブン'"
-                        >1974年 - 現在</v-col
-                    >
-                    <v-col align="center" class="xs-font pt-1 pb-0" v-else-if="hotsnack.store === 'LAWSON'"
-                        >1975年 - 現在</v-col
-                    >
-                    <v-col align="center" class="xs-font pt-1 pb-0" v-else-if="hotsnack.store === 'FamilyMart'"
-                        >1973年 - 現在</v-col
-                    >
-                    <v-col align="center" class="xs-font pt-0 pb-2"
-                        >{{ hotsnack.genre }} / {{ hotsnack.ingredient }}</v-col
-                    >
-                </v-card>
+                    <v-card raised elevation="8" tile class="mx-10 my-10">
+                        <v-col align="center" class="s-font pt-2 pb-0">{{ hotsnack.name }}</v-col>
+                        <v-col align="center" class="xs-font pt-1 pb-2">{{ hotsnack.price }}円（税込）</v-col>
+                        <hr class="hr2" />
+                        <v-col align="center" class="s-font pt-2 pb-0">{{ hotsnack.store }}</v-col>
+                        <v-col align="center" class="xs-font pt-1 pb-0" v-if="hotsnack.store === 'セブン-イレブン'"
+                            >1974年 - 現在</v-col
+                        >
+                        <v-col align="center" class="xs-font pt-1 pb-0" v-else-if="hotsnack.store === 'LAWSON'"
+                            >1975年 - 現在</v-col
+                        >
+                        <v-col align="center" class="xs-font pt-1 pb-0" v-else-if="hotsnack.store === 'FamilyMart'"
+                            >1973年 - 現在</v-col
+                        >
+                        <v-col align="center" class="xs-font pt-0 pb-2"
+                            >{{ hotsnack.genre }} / {{ hotsnack.ingredient }}</v-col
+                        >
+                    </v-card>
 
-                <v-card-actions>
-                    <v-col class="pa-0">
-                        作品解説
-                        <v-btn icon>
-                            <v-icon icon>mdi-volume-high</v-icon>
+                    <v-card-actions>
+                        <v-col class="pa-0">
+                            作品解説
+                            <v-btn icon>
+                                <v-icon icon>mdi-volume-high</v-icon>
+                            </v-btn>
+                        </v-col>
+                        <v-spacer></v-spacer>
+
+                        <v-btn icon @click="show = !show">
+                            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
                         </v-btn>
-                    </v-col>
-                    <v-spacer></v-spacer>
+                    </v-card-actions>
 
-                    <v-btn icon @click="show = !show">
-                        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                    </v-btn>
-                </v-card-actions>
-
-                <v-expand-transition>
-                    <div v-show="show">
-                        <hr class="hr1" />
-                        <v-card-text>{{ hotsnack.description }}</v-card-text>
-                    </div>
-                </v-expand-transition>
+                    <v-expand-transition>
+                        <div v-show="show">
+                            <hr class="hr1" />
+                            <v-card-text>{{ hotsnack.description }}</v-card-text>
+                        </div>
+                    </v-expand-transition>
+                </v-card>
             </v-card>
-        </v-card>
-        <div v-else></div>
+            <p v-if="!hotsnack.id">美術品は見つかりませんでした</p>
+        </base-transition>
     </base-container>
 </template>
 
 <script>
+import BaseTransition from '../components/atom/transitions/BaseTransition.vue'
 import { BaseContainer } from '../components/layout'
 
 export default {
     name: 'HotsnackDetail',
     components: {
         BaseContainer,
+        BaseTransition,
     },
     data() {
         return {
             show: true,
             hotsnack: {},
+            isLoading: false,
         }
     },
     computed: {
@@ -136,6 +141,7 @@ export default {
     },
     methods: {
         getHotsnackDetail() {
+            this.isLoading = true
             const id = this.$route.params.item_uuid
             this.$axios.get(`/hotsnack/${id}`).then(({ data }) => {
                 // APIレスポンスのデータがないときは、dataプロパティの初期値を設定
@@ -143,15 +149,13 @@ export default {
                     return (this.hotsnack = {})
                 }
                 this.hotsnack = data
+                this.isLoading = false
             })
         },
         addLikeCount() {
             const id = this.$route.params.item_uuid
             this.$axios
-                .put(`/hotsnack/${id}`, {
-                    // parameter 定数
-                    like_count: this.hotsnack.like_count + 1,
-                })
+                .put(`/hotsnack/${id}/like`)
                 .then((res) => {
                     if (res.status === 200) {
                         // ローカルのデータプロパティ
@@ -163,9 +167,7 @@ export default {
         addDislikeCount() {
             const id = this.$route.params.item_uuid
             this.$axios
-                .put(`/hotsnack/${id}`, {
-                    dislike_count: this.hotsnack.dislike_count + 1,
-                })
+                .put(`/hotsnack/${id}/dislike`)
                 .then((res) => {
                     if (res.status === 200) {
                         this.hotsnack.dislike_count++
@@ -186,7 +188,7 @@ export default {
     padding: 6%;
     box-shadow: inset 0px 0.3em 0.1em rgba(0, 0, 0, 0.2);
 }
-.baseline {
+.block {
     display: block;
 }
 .inline {
