4fa63fbe5f8095c3e0f1dbd77c1b59fb7f4a9b1b
eityamo <eityamo@gmail.com>
2022-08-27 23:12:35 +0900

    add:バックエンドの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'random', to: 'hotsnacks#random'
+      get 'hotsnacks', to: 'hotsnacks#index'
       get 'hotsnack/:item_uuid', to: 'hotsnacks#show'
       put 'hotsnack/:item_uuid/like', to: 'likes#create'
       put 'hotsnack/:item_uuid/dislike', to: 'dislikes#create'
