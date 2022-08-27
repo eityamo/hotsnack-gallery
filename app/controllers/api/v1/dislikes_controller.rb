bfa8ae4fb5bd75727850d480e249f4fa93680b42
eityamo <eityamo@gmail.com>
2022-08-27 16:18:03 +0900

    fix:routes.rbの変更

---
+++ app/controllers/api/v1/dislikes_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::DisdislikesController < ApplicationController
+  before_action :set_hotsnack, only: %w[create]
+
+  def create
+    @hotsnack.update(dislike_count: @hotsnack.dislike_count + 1)
+    render json: @hotsnack
+  end
+
+  private
+
+  def set_hotsnack
+    @hotsnack = Hotsnack.find_by(item_uuid: params[:item_uuid])
+  end
+end
